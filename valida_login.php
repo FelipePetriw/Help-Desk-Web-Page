69ed33a176361ec38e4d062976ba26d447f7a4ef
Felipe Petriw <felipe.petriw.ads@gmail.com>
2023-05-14 20:34:50 -0300

    autenticação de usuários
    Aplicação de função para autenticação do usuário e senha utilizado no login. (comparação com os logins cadastrados)

--- valida_login.php
+++ valida_login.php
@@ -1,9 +1,26 @@
 <?php
 
-    print_r($_POST);
-    echo '<br />'
+    //Variavel que verifica se a autenticação foi realizada
+    $usuario_autenticado = false;
 
-    echo $_POST['email'];
-    echo '<br />';
-    echo $_POST['senha'];
+    //Ususários do sistema
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+    );
+
+    foreach($usuarios_app as $user) {
+        
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if($usuario_autenticado) {
+        echo 'Autenticado com sucesso!';
+    } else {
+        header('Location: index.php?login=erro');
+    }
+
+    
 ?>
