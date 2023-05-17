f33f1779519d1730196a9b27327b095ce11cf59e
Felipe Petriw <felipe.petriw.ads@gmail.com>
2023-05-17 14:32:54 -0300

    perfis de usuários
    Definição de perfis administrativos e de usuários e realizado a definição de cada tipo sobre os logins cadastrados.

--- valida_login.php
+++ valida_login.php
@@ -6,12 +6,14 @@
     $usuario_autenticado = false;
     $usuario_id = null;
 
+    $perfis = array(1 => 'Administrativo', 2 => 'Usuário')
+
     //Usuários do sistema
     $usuarios_app = array(
-        array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '123456'),
-        array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '123456'),
-        array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '123456'),
-        array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '123456'),
+        array('id' => 1, 'email' => 'adm@teste.com.br', 'senha' => '123456', 'perfil_id' => 1),
+        array('id' => 2, 'email' => 'user@teste.com.br', 'senha' => '123456', 'perfil_id' => 1),
+        array('id' => 3, 'email' => 'jose@teste.com.br', 'senha' => '123456', 'perfil_id' => 2),
+        array('id' => 4, 'email' => 'maria@teste.com.br', 'senha' => '123456', 'perfil_id' => 2),
     );
 
     foreach($usuarios_app as $user) {
